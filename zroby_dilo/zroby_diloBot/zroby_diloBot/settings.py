00f76d73310b9ee4bdec74c2c9398686a3aa464c
DimonAgon <80159089+DimonAgon@users.noreply.github.com>
2023-06-18 23:11:10 +0300

    Update settings.py

--- zroby_dilo/zroby_diloBot/zroby_diloBot/settings.py
+++ zroby_dilo/zroby_diloBot/zroby_diloBot/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-TOKEN = '6183053920:AAE8nWaz2Ov5fwLUUZH8q-dQ_JCNOKVI0Dw'
+TOKEN = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
